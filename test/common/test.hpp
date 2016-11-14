4eeaa98cfb66df3a8f060ab374a98833f6f2d10c
Krzysztof Jusiak <krzysztof@jusiak.net>
2016-11-14 22:24:02 +0000

    [test] cleanup

--- test/common/test.hpp
+++ test/common/test.hpp
@@ -1,4 +1,4 @@
-- include pph.hpp  //
+//
 // Copyright (c) 2016 Krzysztof Jusiak (krzysztof at jusiak dot net)
 //
 // Distributed under the Boost Software License, Version 1.0.
@@ -14,8 +14,7 @@
   (void)((__VA_ARGS__) || (expect_fail__(#__VA_ARGS__, __FILE__, __LINE__), 0))
 #define static_expect(...) static_assert((__VA_ARGS__), "fail")
 
-    void
-    expect_fail__(const char* msg, const char* file, int line) {
+void expect_fail__(const char* msg, const char* file, int line) {
   std::printf("%s:%d:%s\n", file, line, msg);
   std::exit(-1);
 }
